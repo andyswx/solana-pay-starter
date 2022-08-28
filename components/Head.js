37c6ce2a110e802d5d196c1a25ef4b4452148918
andyswx <andrew.szwec+github@gmail.com>
2022-08-28 20:37:43 +0200

    working sol pay"

--- components/Head.js
+++ components/Head.js
@@ -7,23 +7,23 @@ export default function HeadComponent() {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="theme-color" content="#000000" />
 
-      <title>Solana Pay Store</title>
-      <meta name="title" content="Solana Pay Store" />
-      <meta name="description" content="Buy items on my store using Solana Pay!" />
+      <title>SolCaps</title>
+      <meta name="title" content="SolCaps" />
+      <meta name="description" content="Caps and Snapbacks bought with Solana." />
 
       {/* Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://buildspace.so/" />
-      <meta property="og:title" content="Solana Pay Store" />
-      <meta property="og:description" content="Buy items on my store using Solana Pay!" />
-      <meta property="og:image" content="https://cdn.buildspace.so/courses/solana-pay/metadata.png" />
+      <meta property="og:url" content="https://solcaps.xyz/" />
+      <meta property="og:title" content="SolCaps" />
+      <meta property="og:description" content="Caps and Snapbacks bought with Solana." />
+      <meta property="og:image" content="https://miro.medium.com/max/1200/1*bPFhA3IXnFU_Q_oITCNzEw.png" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://buildspace.so/" />
-      <meta property="twitter:title" content="Solana Pay Store" />
-      <meta property="twitter:description" content="Buy items on my store using Solana Pay!" />
-      <meta property="twitter:image" content="https://cdn.buildspace.so/courses/solana-pay/metadata.png" />
+      <meta property="twitter:url" content="https://solcaps.xyz/" />
+      <meta property="twitter:title" content="SolCaps" />
+      <meta property="twitter:description" content="Caps and Snapbacks bought with Solana." />
+      <meta property="twitter:image" content="https://miro.medium.com/max/1200/1*bPFhA3IXnFU_Q_oITCNzEw.png" />
     </Head>
   );
 }
